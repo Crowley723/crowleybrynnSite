84629474ee03d7dfb06bfd7f11cad852ac73a3ff
Crowley723 <littlehill723@gmail.com>
2023-06-01 15:21:12 -0700

    formatting

--- loadFlowerData.php
+++ loadFlowerData.php
@@ -6,7 +6,7 @@
         
     </head>
     <body>
-        <h1>Flower Data</h1>
+        <h1 style="padding-left:16px">Flower Data</h1>
         <div style="padding-left:16px" id="dataTable"></div>
         <script src="./getFlowerData.js"></script>
     
