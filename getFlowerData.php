413435c665dbc346d75458ce07b59871ee310b25
Crowley723 <littlehill723@gmail.com>
2023-06-01 16:03:04 -0700

    added print statements

--- getFlowerData.php
+++ getFlowerData.php
@@ -11,17 +11,21 @@
     $conn = new new mysqli($hostname, $username, $password, $database);
 
     if($conn -> connect_error){
+        echo "Connect Error";
         die("Database Connection Error, Error No.: ".$conn->connect_errno." | ".$conn->connect_error);
+        
     }
     $lastLoadedRow = $_SERVER['Last-Loaded-Row'];
     $query = "SELECT `ID`, `Sensor`, `Temperature(C)`, `Humidity(%)`, `Timestamp` FROM Flowers WHERE ID > $lastLoadedRow ORDER BY id DESC";
     $result = $conn->query($query);
 
     $data = array();
+    echo $result;
     if($result->num_rows > 0){
         while($row = $result->fetch_assoc()){
             $data[] = $row;
         }
+        
     }
     header('Content-Type: application/json');
     echo var_dump($data);
