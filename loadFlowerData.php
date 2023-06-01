1e152ba762517567253ffc6f51fef922d6989c5b
Crowley723 <littlehill723@gmail.com>
2023-06-01 15:32:17 -0700

    added jquery include

--- loadFlowerData.php
+++ loadFlowerData.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>Dyn. Load Flower Data</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <?php include "./header.php" ?>
         
     </head>
