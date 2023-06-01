d70a3f5625e01f905bd6ea061c6c71208dbd07a9
Crowley723 <littlehill723@gmail.com>
2023-06-01 15:54:33 -0700

    added error reporting for php

--- getFlowerData.php
+++ getFlowerData.php
@@ -1,5 +1,8 @@
 <?php
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
     $hostname = "localhost";
     $database = "sensorData";
     $username = getenv('SQLUSER');
