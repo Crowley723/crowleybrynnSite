a436f71bebdda23618725142db7dffc3cebd7ff5
Crowley723 <littlehill723@gmail.com>
2023-06-01 15:20:52 -0700

    trying to get ajax to load the data.

--- loadFlowerData.php
+++ loadFlowerData.php
@@ -3,10 +3,12 @@
     <head>
         <title>Dyn. Load Flower Data</title>
         <?php include "./header.php" ?>
-        <script src="getFlowerData.js"></script>
+        
     </head>
     <body>
+        <h1>Flower Data</h1>
         <div style="padding-left:16px" id="dataTable"></div>
+        <script src="./getFlowerData.js"></script>
     
     </body>
 </html>
