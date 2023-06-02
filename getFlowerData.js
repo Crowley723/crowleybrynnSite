5bfefc840cbbc1eedefc7930f0cd8f08478493dd
Crowley723 <littlehill723@gmail.com>
2023-06-01 17:02:27 -0700

    commented out console log

--- getFlowerData.js
+++ getFlowerData.js
@@ -18,7 +18,7 @@ jQuery(document).ready(function() {
             dataType: 'json',
             success: function(data){
                 lastLoadedRow = updatePage(data);
-                console.log(data);
+                //console.log(data);
             },
             error: function(xhr, status, error) {
                 console.log(status + ': ' + error);
