0edc37059707f2f405adb44adf2352bed3efd62d
Crowley723 <littlehill723@gmail.com>
2023-06-01 17:01:54 -0700

    added thead and tbody to main page

--- loadFlowerData.php
+++ loadFlowerData.php
@@ -13,14 +13,17 @@ error_reporting(E_ALL);
     <body>
         <h1 style="padding-left:16px">Flower Data</h1>
         <table id="dataTable" cellspacing="5" cellpadding="5" style="padding-left:7px">
-        <tr>
-          <th><b>ID</b></th>
-          <th><b>Temperature(C)</b></th>
-          <th><b>Humidity(%)</b></th>
-          <th><b>Timestamp</b></th>
-        </tr>
+        <thead>
+            <tr>
+            <th><b>ID</b></th>
+            <th><b>Temperature(C)</b></th>
+            <th><b>Humidity(%)</b></th>
+            <th><b>Timestamp</b></th>
+            </tr>
+        </thead>
+            <tbody id="tableBody">
+            </tbody>
         <script src="./getFlowerData.js"></script>
-    
         </table>
     </body>
 </html>
