22c38b68003dd1c63308df3aa88c1623170892df
Crowley723 <littlehill723@gmail.com>
2023-06-27 18:55:30 -0700

    implement lastest-row-unaware ajax script to load data

---
+++ getFlowerDataSimple.js
@@ -0,0 +1,55 @@
+jQuery(document).ready(function(){
+    fetchData();
+    setInterval(function(){
+        fetchData();
+    }, 30000);
+});
+
+function fetchData(){
+    $.ajax({
+        url: 'getFlowerDataSimple.php',
+        type: 'GET',
+        headers:{
+
+        },
+        dataType: 'json',
+        success: function(data) {
+            updatePage(data)
+
+        }, error: function(xhr, status, error){
+            console.log(status + ': ' + error);
+        }
+    })
+}
+
+function updatePage(data, callback) {
+    var table = $('#dataTable');
+    var tableBody = table.find('tbody');
+    if (tableBody.length === 0) {
+        tableBody = $('<tbody>');
+        table.prepend(tableBody); // Prepend the table body to maintain newest item at the top
+    }
+    for (var i = data.length - 1; i >= 0; i--) {
+        var point = data[i];
+
+        console.log('Data: ' + data);
+        console.log('HighestID: ' + highestId);
+        if (
+            point.hasOwnProperty('ID') &&
+            point.hasOwnProperty('Temperature(C)') &&
+            point.hasOwnProperty('Humidity(%)') &&
+            point.hasOwnProperty('Timestamp')
+        ) {
+            // Create and append the HTML for the data point
+            var newRow = $('<tr>');
+            newRow.append('<td>' + point['ID'] + '</td>');
+            newRow.append('<td>' + point['Temperature(C)'] + '</td>');
+            newRow.append('<td>' + point['Humidity(%)'] + '</td>');
+            newRow.append('<td>' + point['Timestamp'] + '</td>');
+
+            // Prepend the new data point to the container
+            tableBody.prepend(newRow);
+        }
+    }
+
+}
---
+++ getFlowerDataSimple.php
@@ -0,0 +1,34 @@
+<?php
+
+    //ini_set('display_errors', 1);
+    //ini_set('display_startup_errors', 1);
+    //error_reporting(E_ALL);
+
+    $hostname = "localhost";
+    $database = "sensorData";
+    $username = getenv('SQLUSER');
+    $password = getenv('SQLPASS');
+
+    $conn = new mysqli($hostname, $username, $password, $database);
+
+    if($conn -> connect_error){
+        die("Database Connection Error, Error No.: ".$conn->connect_errno." | ".$conn->connect_error);
+        
+    }
+    $headers = getallheaders();
+    /*$lastLoadedRow = isset($headers['Last-Loaded-Row']) ? $headers['Last-Loaded-Row'] : null;
+    if(is_null($lastLoadedRow)){
+        $lastLoadedRow = 0;
+    }*/
+    $query = "SELECT `ID`, `Temperature(C)`, `Humidity(%)`, `Timestamp` FROM Flowers WHERE ID > " . $lastLoadedRow . " ORDER BY id DESC";
+    $result = $conn->query($query);
+
+    $data = array();
+    if($result->num_rows > 0){
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+    }
+    header('Content-Type: application/json');
+    echo json_encode($data, JSON_THROW_ON_ERROR);
+
+    $conn->close();
+?>
