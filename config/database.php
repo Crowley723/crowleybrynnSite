7846fdfced2eb29c3c47e78485159b72751c18f4
Brynn Crowley <littlehill723@gmail.com>
2024-08-02 23:19:53 -0700

    fix database error

--- config/database.php
+++ config/database.php
@@ -1,21 +1,25 @@
 <?php
 class Database {
-    private $host = getenv('SQLHOSTNAME');
+    private $host;
     private $dbname = "weather_station";
-    private $username = getenv('SQLUSER');
-    private $password = getenv('SQLPASS');
+    private $username;
+    private $password;
     public $conn;
+
+    public function __construct() {
+        $this->host = getenv('SQLHOSTNAME');
+        $this->username = getenv('SQLUSER');
+        $this->password = getenv('SQLPASS');
+    }
 
     public function getConnection() {
         $this->conn = null;
-
         try {
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->dbname, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
-
         return $this->conn;
     }
 }
